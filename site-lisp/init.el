58b999043ccc342b09849d7965bffde0426f846b
Christian Dietrich <stettberger@dokucode.de>
2019-08-15 12:55:55 +0200

    import site lisp

---
+++ site-lisp/init.el
@@ -0,0 +1,16 @@
+(setq user-init-file (or load-file-name (buffer-file-name)))
+(setq load-dir (file-name-directory user-init-file))
+(add-to-list 'load-path load-dir)
+(add-to-list 'load-path (format "%s/org-plus-contrib-20190701" load-dir))
+
+(message "This is the PSÜ org-mode exporter. Initializing... (%s)" load-dir)
+(toggle-debug-on-error)
+
+(require 'org)
+(require 'ob-tangle-sra)
+
+(setq org-html-klipsify-src t)
+(setq org-html-htmlize-font-prefix  "org-")
+(setq org-html-htmlize-output-type 'css)
+
+(message "... emacs is started.")
