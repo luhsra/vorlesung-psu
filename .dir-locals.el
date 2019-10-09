a2e941da825894c3a30e7d3e531cc1cb06bf8e62
Christian Dietrich <stettberger@dokucode.de>
2019-10-09 13:04:43 +0200

    02: zusammenfassung

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 (
  (org-mode . ((fill-column . 130)
-              (ospl-mode . 1))
+              (ospl-mode . 1)
+              (ispell-local-dictionary ."german8")))
  )
--- site-lisp/init.el
+++ site-lisp/init.el
@@ -15,4 +15,18 @@
 (setq org-html-htmlize-output-type 'css)
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values '((ospl-mode . 1))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 (message "... emacs is started.")
