8cc69a72b7fd97708dc79bfd87d7ea926b78f995
Christian Dietrich <stettberger@dokucode.de>
2019-12-10 13:23:44 +0100

    05: ast nodes

---
+++ lst/05-DeclRefExpr.py
@@ -0,0 +1,15 @@
+#range0
+class MemberExpr(Expr):
+  # AST Kinder:
+  lhs_obj    : Expr
+  rhs_name   : Identifier
+
+  # AST Attribute
+  expr_type  : Type      = None
+  field_decl : FieldDecl = None
+
+  def __init__(self, L, R):
+      self.lhs_obj   = L
+      self.rhs_name  = R
+  ...
+#range1
