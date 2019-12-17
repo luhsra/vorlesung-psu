3d1894006a1579d1aa2f8b45df5451226a0b1620
Christian Dietrich <stettberger@dokucode.de>
2019-12-17 16:49:19 +0100

    05: typinferenz

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,11 @@
-(
- (org-mode . ((fill-column . 130)
-              (ospl-mode . 1)
-              (ispell-local-dictionary ."german8")))
- )
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((TeX-mode
+  (ispell-local-dictionary . "german8"))
+ (org-mode
+  (fill-column . 130)
+  (ospl-mode . 1)
+  (ispell-local-dictionary . "german8"))
+ (tex-mode
+  (ispell-local-dictionary . "german8")))
